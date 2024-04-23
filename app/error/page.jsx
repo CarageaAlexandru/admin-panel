d4bfdb7c6b4a39df33ed34a595454b531da74382
Caragea <caragea.alexandru@yahoo.com>
2024-04-23 18:31:11 +0100

    test

--- app/error/page.jsx
+++ app/error/page.jsx
@@ -1,25 +1,31 @@
 "use client"
-import React from 'react';
-import {useRouter} from 'next/navigation'
-import {useSearchParams} from 'next/navigation'
+import React, {Suspense} from 'react';
+import {useRouter} from 'next/navigation';
+import {useSearchParams} from 'next/navigation';
 import ErrorAlert from "@/app/components/error/error";
 
+const ErrorMessage = () => {
+    const searchParams = useSearchParams();
+    const message = searchParams.get("message");
+    return (
+        message ? (
+            <ErrorAlert message={decodeURIComponent(message)}/>
+        ) : (
+            <ErrorAlert message="Sorry, something went wrong, but no error details were provided."/>
+        )
+    );
+};
+
 const Page = () => {
-    const router = useRouter()
-    const searchParams = useSearchParams()
-    const message = searchParams.get("message")
+    const router = useRouter();
+
     return (
         <div className="flex justify-center items-center h-screen">
             <div className="flex flex-col p-4 shadow-lg rounded max-w-sm w-full items-center">
-
-                {/* Display the error message if it exists, otherwise show a default message */}
-                {message ? (
-                    <ErrorAlert message={decodeURIComponent(message)}/>
-                ) : (
-                    <ErrorAlert message="Sorry, something went wrong, but no error details were provided."/>
-                )}
-                {/* Button to navigate back */}
-                <button className="btn btn-accent mt-4" onClick={router.back}>Go Back</button>
+                <Suspense fallback={<p>Loading...</p>}>
+                    <ErrorMessage/>
+                </Suspense>
+                <button className="btn btn-accent mt-4" onClick={() => router.back()}>Go Back</button>
             </div>
         </div>
     );
