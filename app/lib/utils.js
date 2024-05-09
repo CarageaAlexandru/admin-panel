e973974971e22dc0899251ade04b31310edd5632
Caragea <caragea.alexandru@yahoo.com>
2024-05-09 15:30:21 +0100

    fetching transaction details from postgres function

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -2,6 +2,8 @@ export const formatDate = (date) => {
     return new Date(date).toLocaleDateString("en-US", {
         month: 'long',
         day: 'numeric',
-        year: "numeric"
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit"
     })
 }
--- app/ui/dashboard/transactions/transactions.jsx
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -1,33 +1,12 @@
 import React from 'react';
+import {createClient} from "@/supabase/server";
+import clsx from "clsx";
+import {formatDate} from "@/app/lib/utils";
 
-// Dummy data for transactions
-const transactionsData = [
-    {id: 4, name: 'Jennica Kindred', status: 'done', date: '22.01.2024', amount: '2400'},
-    {id: 5, name: 'Dorolice Crossman', status: 'cancelled', date: '05.02.2024', amount: '3300'},
-    {id: 6, name: 'Bailie Coulman', status: 'pending', date: '18.02.2024', amount: '1900'},
-    {id: 7, name: 'Felice Walkington', status: 'pending', date: '22.02.2024', amount: '4100'},
-    {id: 8, name: 'Emmalynn Penndragon', status: 'pending', date: '14.03.2024', amount: '2950'},
-    {id: 9, name: 'Roscoe Barfoot', status: 'done', date: '28.03.2024', amount: '2250'},
-    {id: 10, name: 'Barby Heisler', status: 'cancelled', date: '09.04.2024', amount: '1850'},
-    {id: 11, name: 'Karylin Stollenbeck', status: 'done', date: '19.04.2024', amount: '5600'},
-    {id: 12, name: 'Dorry Gillingham', status: 'cancelled', date: '23.04.2024', amount: '3200'},
-    {id: 13, name: 'Patty Kneass', status: 'done', date: '30.04.2024', amount: '2450'},
-];
-
-const getStatusBadgeClass = (status) => {
-    switch (status) {
-        case 'done':
-            return 'badge badge-success';
-        case 'cancelled':
-            return 'badge badge-error';
-        case 'pending':
-            return 'badge badge-info';
-        default:
-            return 'badge';
-    }
-};
-
-const Transactions = (props) => {
+export default async function Transactions() {
+    const supabase = createClient()
+    const {data: transactions, error} = await supabase.rpc("transaction_details")
+    console.log(transactions)
     return (
         <div className="flex flex-col max-h-lvh h-lvh">
             <h2 className="text-xl font-semibold">Latest Transactions</h2>
@@ -37,22 +16,38 @@ const Transactions = (props) => {
                     <thead>
                     <tr>
                         <th></th>
-                        <th>Name</th>
+                        <th>Client Name</th>
+                        <th>Product</th>
+                        <th>Price</th>
+                        <th>Total</th>
                         <th>Status</th>
                         <th>Date</th>
-                        <th>Amount</th>
                     </tr>
                     </thead>
                     <tbody>
-                    {transactionsData.map((transaction) => (
+                    {transactions.map((transaction) => (
                         <tr key={transaction.id} className="bg-base-200 hover:bg-base-300">
-                            <td>{transaction.id}</td>
-                            <td>{transaction.name}</td>
+                            <td>{transaction.transaction_id}</td>
+                            <td>{transaction.user_name}</td>
                             <td>
-                                <div className={getStatusBadgeClass(transaction.status)}>{transaction.status}</div>
+                                <div className="indicator">
+                                    <span className="indicator-item badge badge-accent">{transaction.quantity}</span>
+                                    <div
+                                        className="grid h-max w-max p-2 bg-secondary text-white place-items-center rounded-xl">{transaction.product_name}</div>
+                                </div>
                             </td>
-                            <td>{transaction.date}</td>
-                            <td>£ {transaction.amount}</td>
+                            <td>£ {transaction.price}</td>
+                            <td>£ {transaction.total_amount}</td>
+                            <td>
+                                <div className={clsx(
+                                    "badge", {
+                                        "badge badge-info": transaction.status === "pending",
+                                        "badge badge-error": transaction.status === "cancelled",
+                                        "badge badge-success": transaction.status === "completed",
+                                    }
+                                )}>{transaction.status}</div>
+                            </td>
+                            <td>{formatDate(transaction.transaction_date)}</td>
                         </tr>
                     ))}
                     </tbody>
@@ -61,5 +56,3 @@ const Transactions = (props) => {
         </div>
     );
 };
-
-export default Transactions;
